aa748928068dc29a0da8293d30ddba14e6bc4c7f
Alejandro González <info@janogonzalez.com>
2010-12-02 15:05:31 -0300

    Basic routes

--- my-tweet-week.rb
+++ my-tweet-week.rb
@@ -3,11 +3,16 @@ Bundler.setup(:default)
 
 require 'sinatra'
 require 'haml'
+require './models/week-resume'
 
 class MyTweetWeek < Sinatra::Base
   set :haml, :format => :html5, :attr_wrapper => '"'
   
   get '/' do
     haml :index
+  end
+  
+  get '/resume' do
+    haml :resume
   end
 end
