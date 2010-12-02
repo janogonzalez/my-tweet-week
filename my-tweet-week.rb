b67d06c4fec24c4efa978a72175f2f447da3ea43
Alejandro González <info@janogonzalez.com>
2010-12-02 15:08:02 -0300

    require cleanups

--- my-tweet-week.rb
+++ my-tweet-week.rb
@@ -3,7 +3,6 @@ Bundler.setup(:default)
 
 require 'sinatra'
 require 'haml'
-require './models/week-resume'
 
 class MyTweetWeek < Sinatra::Base
   set :haml, :format => :html5, :attr_wrapper => '"'
