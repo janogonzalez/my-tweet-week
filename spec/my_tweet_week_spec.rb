f08f37e1d21d7b76f00e7efb59599dfe0d283d8a
Alejandro González <info@janogonzalez.com>
2010-12-02 15:42:03 -0300

    Testing

---
+++ spec/my_tweet_week_spec.rb
@@ -0,0 +1,29 @@
+require 'bundler'
+Bundler.setup(:default, :test)
+
+require 'rspec'
+require 'rack/test'
+require File.join(File.dirname(__FILE__), '..', 'my_tweet_week.rb')
+
+set :environment, :test
+
+describe "MyTweetWeek" do
+  include Rack::Test::Methods
+
+  def app
+    @app ||= MyTweetWeek
+  end
+  
+  describe "Routes tests" do
+    it "should respond to /" do
+      get '/'
+      last_response.should be_ok
+    end
+
+    it "should respond to /resume" do
+      get '/resume'
+      last_response.should be_ok
+    end
+  end
+end
+
