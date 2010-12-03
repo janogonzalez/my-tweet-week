f886640e00f21d60e9747ef508a26559b849e3a1
Alejandro González <info@janogonzalez.com>
2010-12-03 03:23:14 -0300

    Basic info

--- my_tweet_week.rb
+++ my_tweet_week.rb
@@ -10,56 +10,59 @@ class MyTweetWeek < Sinatra::Base
   set :haml, :format => :html5, :attr_wrapper => '"'
   enable :sessions, :static
   set :public, File.join(File.dirname(__FILE__), 'public')
-  
-  before do
-    @consumer ||= OAuth::Consumer.new(
-      ENV['CONSUMER_KEY'],
-      ENV['CONSUMER_SECRET'],
-      :site => "https://api.twitter.com"
-    )
 
-    if !session[:access_token].nil? && !session[:access_token_secret].nil?
-      @access_token = OAuth::AccessToken.new(
-        @consumer,
-        session[:access_token],
-        session[:access_token_secret]
-      )
-    end
-  end
-  
   get '/' do
     haml :index
   end
   
   get '/login' do
-    @request_token = @consumer.get_request_token(:oauth_callback => ENV['OAUTH_CALLBACK'])
+    request_token = consumer.get_request_token(:oauth_callback => ENV['OAUTH_CALLBACK'])
 
-    session[:request_token] = @request_token.token
-    session[:request_token_secret] = @request_token.secret
+    session[:request_token] = request_token.token
+    session[:request_token_secret] = request_token.secret
     
-    redirect @request_token.authorize_url
+    redirect request_token.authorize_url
   end
   
   get '/oauth_callback' do
-    @request_token = OAuth::RequestToken.new(
-      @consumer,
+    request_token = OAuth::RequestToken.new(
+      consumer,
       session[:request_token],
       session[:request_token_secret]
     )
     
-    session[:request_token] = session[:request_token_secret] = nil 
+    session[:request_token] = session[:request_token_secret] = nil
     
-    @access_token = @request_token.get_access_token(:oauth_verifier => params[:oauth_verifier])
+    access_token = request_token.get_access_token(:oauth_verifier => params[:oauth_verifier])
+
+    Twitter.configure do |config|
+      config.consumer_key = ENV['CONSUMER_KEY']
+      config.consumer_secret = ENV['CONSUMER_SECRET']
+      config.oauth_token =  access_token.token
+      config.oauth_token_secret = access_token.secret
+    end
     
-    session[:access_token] = @access_token.token
-    session[:access_token_tsecret] = @access_token.secret
+    session[:authenticated] = true
     
     redirect '/resume'
   end
   
   get '/resume' do
-    @client = Twitter::Client.new(@access_token)
+    @client = Twitter::Client.new
+    
+    @screen_name = @client.verify_credentials.screen_name
+    @number_of_tweets = 0
 
     haml :resume
+  end
+  
+  private
+  
+  def consumer
+    @consumer ||= OAuth::Consumer.new(
+      ENV['CONSUMER_KEY'],
+      ENV['CONSUMER_SECRET'],
+      :site => "https://api.twitter.com"
+    )
   end
 end
